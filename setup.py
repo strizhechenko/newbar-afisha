fdb3715f172ff6f79325f87f66ab06e4f0fff1ec
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-05-04 16:36:03 +0500

    Fixed: setup.py didn't work correctly.

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from distutils.core import setup
+import os
+from setuptools import setup
 
 setup(
     name='newbar-afisha',
-    version='0.0.2',
+    version='0.0.5',
     packages=['newbar_afisha'],
     url='https://github.com/strizhechenko/newbar-afisha',
     license='GPL',
     author='Oleg Strizhechenko',
     author_email='oleg.strizhechenko@gmail.com',
     description='CLI util for quick-look for recent events in New Bar (ekb)',
-    scripts='scripts/newbar-afisha',
-    install_requires=['requests', 'beautifulsoup4']
+    scripts=['scripts/newbar-afisha'],
+    install_requires=['requests', 'beautifulsoup4'],
+    long_description=(open('README.rst').read())
 )
